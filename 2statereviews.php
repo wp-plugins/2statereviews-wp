a10912a05f2d71ceda1b032c581faf155958532a
darrenwatt <darrenwatt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-19 15:11:29 +0000

    Updated to version 1.1, using API calls.
    git-svn-id: https://plugins.svn.wordpress.org/2statereviews-wp/trunk@825600 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- 2statereviews.php
+++ 2statereviews.php
@@ -1,71 +1,122 @@
 <?php
-/*
-Plugin Name: 2StateReviews-WP
-Plugin URI: http://2statereviews.com/
-Description: 2StateReviews Badge Plugin
-Author: Darren Watt / James Lloyd
-Version: 1.0
-Author URI: http://2statereviews.com/
-*/
+/**
+ * Plugin Name: 2State Widget
+ * Plugin URI: http://2statereviews.com/wordpress/
+ * Description: A widget to display user votes.
+ * Version: 1.1
+ * Author: Darren/James @ 2StateReviews
+ * Author URI: http://2statereviews.com
+ *
+ */
 
-function widget_my2statereviews($args) {
-  extract($args);
-   $options = get_option("widget_my2statereviews");
-    if (!is_array($options))
-        {
-        $options = array(
-        'title' => '2StateReviews'
-        );
-        }  
-  echo $before_widget;
-  echo $before_title;
-   echo $options['title'];
-   echo $after_title;
-  sample2statereviews();
-  echo $after_widget;
+// defining url here as it's handy to switch between dev/live.
+$GLOBALS['twostate_url'] = 'http://2statereviews.com';
+
+
+add_action( 'widgets_init', 'example_load_widgets' );
+
+function example_load_widgets() {
+	register_widget( 'TwoState_Widget' );
 }
 
-function sample2statereviews()
-{
-    $options = get_option("widget_my2statereviews");
-    echo '<center><a href="http://2statereviews.com"><img src="http://2statereviews.com/blogbadge/';
-    echo $options['2stateusername']; 
-    echo '/single.png"></a></center>';
+class TwoState_Widget extends WP_Widget {
+
+	function TwoState_Widget() {
+		
+		$widget_ops = array( 'classname' => 'example', 'description' => __('A widget to display 2StateReviews user votes.', 'example') );
+
+		
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'example-widget' );
+
+		
+		$this->WP_Widget( 'example-widget', __('2State Widget', 'example'), $widget_ops, $control_ops );
+	}
+
+	
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		
+		$title = apply_filters('widget_title', $instance['title'] );
+		$name = $instance['name'];
+		$items = $instance['items'];
+
+		
+		echo $before_widget;
+
+		
+		if ( $title )
+			echo $before_title . $title . $after_title;
+
+		
+		$json = file_get_contents($GLOBALS['twostate_url']."/api/latestuservotes/".$name."/".$items);
+		$array = json_decode($json,TRUE);
+
+		for ($i=0; $i<count($array['votes']); $i++) :
+			$title = $array['votes'][$i]['title'];
+			$poster = $array['votes'][$i]['poster'];
+			$vote =  $array['votes'][$i]['vote'];
+			$movieid = $array['votes'][$i]['movieid'];
+		
+			if ($vote == '1'):
+				$vote = "SEE!";
+			else:
+				$vote = "DON'T SEE!";
+			endif;
+		
+			echo 	$title . "<br>" .
+				"<a href='".$GLOBALS['twostate_url']."/film/" . $movieid . "'>" .
+				"<img src='" .
+				$poster . "'></a>" .
+				$vote . "<hr>";	
+		endfor;
+	
+		echo "All my votes " .
+ 		"<a href='".$GLOBALS['twostate_url']."/user/user_lookup/" . $name . "'>here</a>.";
+
+		
+		echo $after_widget;
+	}
+
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		
+		$instance['title'] = strip_tags( $new_instance['title'] );
+		$instance['name'] = strip_tags( $new_instance['name'] );	
+		$instance['items'] = strip_tags( $new_instance['items'] );
+
+		return $instance;
+	}
+
+	
+	function form( $instance ) {
+
+		
+		$defaults = array( 'title' => __('Movie Reviews', 'example'), 'name' => __('2', 'example'), 'items' => __('3','example') );
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+		
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title:', 'hybrid'); ?></label>
+			<input id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" style="width:100%;" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id( 'name' ); ?>"><?php _e('Your 2StateReviews Userid:', 'example'); ?></label>
+			<input id="<?php echo $this->get_field_id( 'name' ); ?>" name="<?php echo $this->get_field_name( 'name' ); ?>" value="<?php echo $instance['name']; ?>" style="width:100%;" />
+		</p>
+
+                <p>
+                        <label for="<?php echo $this->get_field_id( 'items' ); ?>"><?php _e('Number of reviews to fetch:', 'example'); ?></label>
+                        <input id="<?php echo $this->get_field_id( 'items' ); ?>" name="<?php echo $this->get_field_name( 'items' ); ?>" value="<?php echo $instance['items']; ?>" style="width:100%;" />
+                </p>
+
+
+	<?php
+	}
 }
 
-function my2statereviews_control()
-{
-      $options = get_option("widget_my2statereviews");
-      if (!is_array( $options ))
-            {
-            $options = array(
-            'title' => '2StateReviews',
-            '2stateusername' => 'Darren'
-            );
-            }  
-    
-       if ($_POST['my2statereviews-Submit'])
-            {
-            $options['title'] = htmlspecialchars($_POST['my2statereviews-WidgetTitle']);
-            $options['2stateusername'] = htmlspecialchars($_POST['my2statereviews-WidgetUsername']);
-            update_option("widget_my2statereviews", $options);
-            }
-    
-?>   <p>
-    <label for="my2statereviews-WidgetTitle">Title: </label>
-    <input type="text" id="my2statereviews-WidgetTitle" name="my2statereviews-WidgetTitle" value="<?php echo $options['title'];?>" />                                                    
-    <label for="my2statereviews-WidgetUsername">2StateReviews Username: </label> 
-    <input type="text" id="my2statereviews-WidgetUsername" name="my2statereviews-WidgetUsername" value="<?php echo $options['2stateusername'];?>" />
-    <input type="hidden" id="my2statereviews-Submit" name="my2statereviews-Submit" value="1" /></p>
-    <?php
-}
-
-function my2statereviews_init()
-{
-  register_sidebar_widget(__('2StateReviews'), 'widget_my2statereviews');
-  register_widget_control(   '2StateReviews', 'my2statereviews_control', 200, 200 );
-}
-
-add_action("plugins_loaded", "my2statereviews_init");
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,15 +1,14 @@
 === Plugin Name ===
 Contributors: darrenwatt
-Donate link: www.darrenwatt.com
-Tags: widget, plugin, sidebar
-Requires at least: 2.0.2
-Tested up to: 2.7
-Stable tag: 1.0
+Donate link: 2statereviews.com
+Tags: widget, plugin, sidebar, movies
+Requires at least: 3.2
+Tested up to: 3.8
+Stable tag: 1.1
 
 == Description ==
 
 This is the 2StateReviews.com Wordpress Widget, which links the latest vote you cast to your blog. 2StateReviews is a review systems where things are either good or bad, no middle ground.
-
 
 == Installation ==
 
@@ -17,8 +16,7 @@ Installation is straight forward.
 
 1. Install in your plugins directory
 2. Activate it in the plugins menu
-3. Edit the 2StateReviews in Widget Menu to your 2StateReviews username and whatever you want the badge to be titled on your wordpress page.
-
+3. Edit the 2StateReviews in Widget Menu to your 2StateReviews userid (you can get this from url when logged into 2statereviews.com) and whatever you want the badge to be titled on your wordpress page.
 
 == Frequently Asked Questions ==
 
